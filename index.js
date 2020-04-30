27b44f04233a0ac4ff9db5e59d582b011539b5f7
Brian Salemi <briandsalemi@gmail.com>
2020-04-30 16:50:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ function bfs(rootNode, vertices, edges){
 
 }
 
-function findAdjacent(nodeName, vertices, edges){
-
+function findAdjacent(nodeName,  vertices, edges){
+  return edges.filter((edge) => {
+    return edge.includes(nodeName)
+  }).map((edge) => {
+    return edge.filter((node)=> {
+      return (node != nodeName)
+    })[0]
+  }).map((name) => {
+    return findNode(name, vertices)
+  }).filter((node) => {
+    return node.distance == null;
+  })
 }
-
 function markDistanceAndPredecessor(predecessor, adjacentNodes){
   adjacentNodes.map((node) => {
     node.distance = predecessor.distance + 1;
