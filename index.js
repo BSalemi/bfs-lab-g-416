08de2a0d16840ed279ac2fe17760fd209e11f900
Brian Salemi <briandsalemi@gmail.com>
2020-04-30 16:48:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,8 @@ function findAdjacent(nodeName, vertices, edges){
 }
 
 function markDistanceAndPredecessor(predecessor, adjacentNodes){
-
+  adjacentNodes.map((node) => {
+    node.distance = predecessor.distance + 1;
+    node.predecessor = predecessor
+  })
 }
